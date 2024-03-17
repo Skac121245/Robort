5a01b9bda5f400c622a2a79e80d6f06acd558f4d
Ben Skraly <benskraly@gmail.com>
2024-03-16 20:54:40 -0400

    updated index.js with new udateign features

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ const createWindow = () => {
   })
 
 console.log("what up dawg")
+
+const { updateElectronApp, UpdateSourceType } = require('update-electron-app')
+updateElectronApp({
+  updateSource: {
+    type: UpdateSourceType.ElectronPublicUpdateService,
+    repo: 'https://github.com//Skac121245Robort.git'
+  },
+  updateInterval: '1 hour',
+  logger: require('electron-log')
+})
