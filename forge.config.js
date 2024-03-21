5384379a84f7c4716454fb5ed133b89f43c1aeb2
Skac121245 <benskraly@gmail.com>
2024-03-21 19:12:58 -0400

    init github actions publishing

--- forge.config.js
+++ forge.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'win64'],
     },
     {
       name: '@electron-forge/maker-deb',
