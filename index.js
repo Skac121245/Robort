e4a4a203a0bbdc71ec485aecb0c4168e41276df7
tee-bee <teebeewiiu@gmail.com>
2024-03-23 10:53:30 -0400

    Added electron indicator to nav bars and implemented Electron localStorage for pre-match checklist

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 2000,
-      height: 1775
+      height: 1775,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false // It's often recommended to keep contextIsolation enabled for security purposes, but it requires additional setup to expose APIs to the renderer.
+    }
     })
   
     win.loadFile('index.html')
