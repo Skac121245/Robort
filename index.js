9739c6f78ccb7c4d61d36aa40a648d68d9892384
Emily Charles <emily.w.charles@gmail.com>
2024-04-09 23:38:05 -0400

    partial implementation of sqlite demo with sqlite-electron

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { setdbPath } = require("sqlite-electron");
 
+const { join } = require("path");
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 2000,
-      height: 1775,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false, // It's often recommended to keep contextIsolation enabled for security purposes, but it requires additional setup to expose APIs to the renderer.
-        devTools: false
-      }
-    })
-  
-    win.loadFile('index.html')
+  const win = new BrowserWindow({
+    width: 2000,
+    height: 1775,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true, // It's often recommended to keep contextIsolation enabled for security purposes, but it requires additional setup to expose APIs to the renderer.
+      devTools: true,
+      preload: join(__dirname, "preload.js"),
+    },
+  });
 
-    win.setMenuBarVisibility(false);
+  win.loadFile("index.html");
+
+  win.setMenuBarVisibility(true);
+};
+
+app.on("ready", function () {});
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+ipcMain.handle("potd", async (event, dbPath, isuri) => {
+  try {
+    return await setdbPath(dbPath, isuri);
+  } catch (error) {
+    console.log(error);
+    return error;
   }
+});
 
-  app.whenReady().then(() => {
-    createWindow()
-  })
-
-console.log("what up dawg")
-
+console.log("what up dawg");
