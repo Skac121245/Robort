49b600dbb881be0de0ab6bdee52ef7c2074cc3b4
Emily Charles <emily.w.charles@gmail.com>
2024-04-12 22:37:43 -0400

    fully working database demo

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { setdbPath } = require("sqlite-electron");
-
+const { app, BrowserWindow, ipcMain, dialog } = require("electron/main");
+const sqlite = require("sqlite-electron");
 const { join } = require("path");
+
+const randomWords = [
+  "kwarqs",
+  "Ben",
+  "Toba",
+  "duck",
+  "robots",
+  "spot",
+  "lamp",
+  "table",
+  "tv",
+];
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,24 +22,71 @@ const createWindow = () => {
       nodeIntegration: true,
       contextIsolation: true, // It's often recommended to keep contextIsolation enabled for security purposes, but it requires additional setup to expose APIs to the renderer.
       devTools: true,
-      preload: join(__dirname, "preload.js"),
+      // this preload line is like importing your API definitions.
+      preload: join(__dirname, "./sqliteDemo/preload.js"),
     },
   });
 
   win.loadFile("index.html");
 
-  win.setMenuBarVisibility(true);
+  win.setMenuBarVisibility(false);
 };
 
-app.on("ready", function () {});
+// This is responsible for showing the file dialog and letting you pick a file.
+async function handleFileOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog();
+  if (!canceled) {
+    return filePaths[0];
+  }
+}
 
 app.whenReady().then(() => {
   createWindow();
 });
 
-ipcMain.handle("potd", async (event, dbPath, isuri) => {
+// This is where you define your back end handlers (what you'll call in preload.js)).
+ipcMain.handle("dialog:openFile", handleFileOpen);
+
+ipcMain.handle("databasePath", async (_event, dbPath) => {
+  return (async () => {
+    try {
+      // Connect
+      await sqlite.setdbPath(`file:${dbPath}?mode:rw`, true);
+      await sqlite.executeQuery(
+        "CREATE TABLE IF NOT EXISTS randomWords (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, word TEXT);"
+      );
+      return "Successfully connected to database!";
+    } catch (err) {
+      console.log(err);
+    }
+  })();
+});
+
+ipcMain.handle("add-random", async (_event) => {
   try {
-    return await setdbPath(dbPath, isuri);
+    const randomInt = Math.floor(Math.random() * randomWords.length);
+    const ret = await sqlite.executeQuery(
+      `INSERT INTO randomWords (word) VALUES ("${randomWords[randomInt]}");`
+    );
+    return ret;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+});
+
+ipcMain.handle("clear-db", async (_event) => {
+  try {
+    return await sqlite.executeQuery(`DELETE FROM randomWords;`);
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+});
+
+ipcMain.handle("get-data", async (_event) => {
+  try {
+    return await sqlite.fetchAll(`SELECT * FROM randomWords;`);
   } catch (error) {
     console.log(error);
     return error;
---
+++ sqliteDemo/renderer.js
@@ -0,0 +1,82 @@
+/*
+In this file, you can talk to stuff that's in the browser, like buttons and
+input fields. When you get input, you can send it to the back end via the window.<blah> functions.
+*/
+
+const openFileButton = document.getElementById("open-file-button");
+const filePathElement = document.getElementById("filePath");
+const connectDbButton = document.getElementById("connect-db");
+const addRecordButton = document.getElementById("add-record");
+const clearDbButton = document.getElementById("clear-db");
+
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+const resultsContainer = document.getElementById("table-container");
+
+// find db file.
+// This calls a function inside "preload.js" called openFile.
+openFileButton.addEventListener("click", async () => {
+  const filePath = await window.fileApi.openFile();
+  filePathElement.innerText = filePath;
+  await window.sqliteApi.path(filePathElement.innerText);
+  reMakeTable();
+});
+
+// create db connection
+connectDbButton.addEventListener("click", async () => {
+  await window.sqliteApi.path(filePathElement.innerText);
+});
+
+// Add listener to add button
+addRecordButton.addEventListener("click", async () => {
+  await window.sqliteApi.add();
+  await reMakeTable();
+});
+
+// Add listener to clear button
+clearDbButton.addEventListener("click", async () => {
+  await window.sqliteApi.clear();
+  await reMakeTable();
+});
+
+// helper function for below
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+// Show results from database, polling every 1000ms
+window.addEventListener("onDOMContentLoaded", () => {});
+
+async function reMakeTable() {
+  const data = await window.sqliteApi.getData();
+  const table = document.createElement("table");
+  table.style.width = "100%";
+  table.setAttribute("border", "1");
+
+  // Make the header row
+  const headerRow = table.insertRow();
+  const headers = ["id", "Random Word"];
+  headers.forEach((headerText) => {
+    const headerCell = document.createElement("th");
+    headerCell.textContent = headerText;
+    headerRow.appendChild(headerCell);
+  });
+
+  // Create a row for each word
+  data.forEach((datum) => {
+    const row = table.insertRow();
+    Object.values(datum).forEach((text) => {
+      const cell = row.insertCell();
+      cell.textContent = text;
+    });
+  });
+
+  // remove and recreate the table
+  removeAllChildren(resultsContainer);
+  resultsContainer.appendChild(table);
+}
