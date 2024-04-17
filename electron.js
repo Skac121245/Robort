c5709e00af7db8ff28645a2e1b7a6cb03a3964a3
tee-bee <teebeewiiu@gmail.com>
2024-04-17 15:11:03 -0400

    Fixing dropdowns that weren't working

--- electron.js
+++ electron.js
@@ -1,11 +1,11 @@
-// localStorage stuff
-document.addEventListener('DOMContentLoaded', (event) => {
-if (typeof window !== 'undefined' && window.process && window.process.type === 'renderer') {
-const navBar = document.querySelector('nav');
-const indicator = document.createElement('span');
-indicator.textContent = ' (Electron App)';
-indicator.style.color = 'white';
-navBar.appendChild(indicator);
-}
-});
+// // localStorage stuff
+// document.addEventListener('DOMContentLoaded', (event) => {
+// if (typeof window !== 'undefined' && window.process && window.process.type === 'renderer') {
+// const navBar = document.querySelector('nav');
+// const indicator = document.createElement('span');
+// indicator.textContent = ' (Electron App)';
+// indicator.style.color = 'white';
+// navBar.appendChild(indicator);
+// }
+// });
 
